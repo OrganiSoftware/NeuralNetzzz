a7aca72dd47377dff283ce9f9bae5390088cfd37
TankyHeals <tankyheals@gmail.com>
2022-12-18 03:45:13 -0600

    coded Neurallayer
    coded NeuralLayer

---
+++ NeuralLayer.py
@@ -0,0 +1,20 @@
+from Preceptron import Preceptron
+
+
+class NeuralLayer:
+    def __init__(self, num_inputs, num_preceptrons, activation_funct, learning_rate):
+        self.neural_layer = []
+        self.num_preceptrons = num_preceptrons
+        for preceptron_index in range(self.num_preceptrons):
+            preceptron = Preceptron(activation_funct, num_inputs, learning_rate)
+            self.neural_layer.append(preceptron)
+
+    def activations(self):
+        activations = []
+        for preceptron_index in range(len(self.neural_layer)):
+            activations.append(self.neural_layer[preceptron_index].activate())
+        return activations
+
+    def preceptron_at_index(self, preceptron_index):
+        return self.neural_layer[preceptron_index]
+
