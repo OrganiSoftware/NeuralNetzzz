5b69c1870d9389b0eb1c046b940e042bec45aeed
Antonio Bruce Webb <120767438+OrganiSoftware@users.noreply.github.com>
2023-10-31 03:22:27 -0500

    added some logic to function parser and flowed through the implementation of it

--- FunctionParser.py
+++ FunctionParser.py
@@ -2,6 +2,7 @@
 @author Antonio Bruce Webb(Organi)
 """
 self.numbers = ["0","1","2","3","4","5","6","7","8","9"]
+self.operations =["+", "-", "/", "*", "^", "nthroot", "riemannsum"]
 class FunctionParser:
 
     def __init__(self, function):
