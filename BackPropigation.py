10d55dbca3faebd091f0ea9980d42389167a6744
Antonio Webb <120767438+OrganiSoftware@users.noreply.github.com>
2024-01-19 04:50:16 -0600

    safasfd

--- BackPropigation.py
+++ BackPropigation.py
@@ -1,3 +1,4 @@
 """
 @author Antonio Bruce Webb(Organi)
 """
+
--- MSECostFunction.py
+++ MSECostFunction.py
@@ -5,18 +5,22 @@ class MSECostFunction:
         self.neural_net = neural_net
         self.training_set = training_set
 
-    def calc_total_cost(self):
+    def calc_total_cost_of_all_states(self):
         sum_cost = 0
         for training_state in self.training_set:
             self.neural_net.load_inputs(training_state.inputs)
             network_activations = []
+            predicted_outputs = []
+            cost_of_state = 0
             for neural_layer in range(len(self.neural_net.neural_net)):
                 layer_activations = self.neural_net.neural_net[neural_layer].activations()
                 network_activations.append(layer_activations)
                 predicted_outputs = network_activations[len(network_activations) - 1]
-            for preceptron in range(len(self.neural_net.neural_net[len(self.neural_net.neural_net) - 1].neural_layer)):
-                
-                sum_cost +=  (1/2*len(preceptron.weights))
+            for perceptron in range(len(predicted_outputs)):
+                cost_of_state += (self.training_set[perceptron] - predicted_outputs[perceptron])**2
+            cost_of_state = cost_of_state * (1/(2*len(predicted_outputs)))
+            sum_cost += cost_of_state
+
 
 
 
