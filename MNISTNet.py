04125ea2a84d59efa54e8368b2d3ee00e98d637b
Antonio Webb <120767438+OrganiSoftware@users.noreply.github.com>
2024-06-19 05:54:41 -0500

    xdfdgvbxsdfgh

--- MNISTNet.py
+++ MNISTNet.py
@@ -21,12 +21,12 @@ def main():
             num_inputs = len(train_data_set.inputs[train_data_state])
             break
     neural_net = NeuralNetwork(output_translation_table, num_inputs, sigmoid, .1)
-    neural_net.add_input_layer(64)
-    neural_net.add_hidden_layers(1, 32)
-    neural_net.add_hidden_layers(1, 64)
+    neural_net.add_input_layer(100)
+    neural_net.add_hidden_layers(1, 100)
+    neural_net.add_hidden_layers(1, 100)
     neural_net.is_constructed()
     mse_optimizer = MSEOptimizer(neural_net, train_data_set)
-    neural_net = mse_optimizer.train(100,64)
+    neural_net = mse_optimizer.train(1000,64)
     neural_net.save_weights_biases("/run/media/jackal/Work/SoftwareProjects/NeuralNetzzz/weights_bias.json")
     count = 0
     for inputs in range(len(test_data_set.expected_outputs)):
